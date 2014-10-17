857adb09f4825987dd9f7b719dc09c9480b5e2b4
Ioann Volkov <volkov.ioann@gmail.com>
2014-10-17 15:29:18 +0400

    Random forest implemented but not tested yet

--- practice_2/random_forest/random_forest.cpp
+++ practice_2/random_forest/random_forest.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 
-#define TREES_COUNT           10
+#define TREES_COUNT           11
 #define RANDOM_INDEXES_COUNT  100
 
 void readData(std::string const& fileName, std::vector<int>* data) {
@@ -40,20 +40,24 @@ public:
         , right(0)
         , fIndex(-1)
     {
-        bool hasMinusOne = false;
-        bool hasPlusOne = false;
+        int minusOnesCount = 0;
+        int plusOnesCount = 0;
         for (int i = 0; i < sampleIndexes.size(); ++i) {
-            if (trainLabels[sampleIndexes[i]] == 1)
-                hasPlusOne = true;
             if (trainLabels[sampleIndexes[i]] == -1)
-                hasMinusOne = true;
+                ++minusOnesCount;
+            else if (trainLabels[sampleIndexes[i]] == 1)
+                ++plusOnesCount;
+            else
+                assert(false);
         }
 
-        assert(hasMinusOne || hasPlusOne);
+        assert(minusOnesCount > 0 || plusOnesCount > 0);
 
-        leaf = (!hasMinusOne || !hasPlusOne);
-        if (leaf)
+        leaf = (minusOnesCount == 0 || plusOnesCount == 0);
+        if (leaf) {
+            fMult = (minusOnesCount > plusOnesCount) ? -1 : 1;
             return;
+        }
 
         std::vector<int> featureIndexes;
         for (int i = 0; i < RANDOM_INDEXES_COUNT; ++i) {
@@ -69,17 +73,79 @@ public:
             }
 
             for (std::set<int>::const_iterator it = featureValues.begin(); it != featureValues.end(); ++it) {
-                if (gini() > bestGini) {
-                    fIndex = featureIndex;
-                    fDivVal = *it;
+                for (int featureMult = -1; featureMult < 2; featureMult += 2) {
+                    int correctMinusOnes = 0;
+                    int wrongMinusOnes = 0;
+                    int correctPlusOnes = 0;
+                    int wrongPlusOnes = 0;
+                    for (size_t k = 0; k < sampleIndexes.size(); ++k) {
+                        int sampleIndex = sampleIndexes[k];
+                        if (trainLabels[sampleIndex] == -1) {
+                            if (trainData[sampleIndex][featureIndex] * featureMult < (*it) * featureMult)
+                                ++correctMinusOnes;
+                            else
+                                ++wrongMinusOnes;
+                        } else if (trainLabels[sampleIndex] == 1) {
+                            if (trainData[sampleIndex][featureIndex] * featureMult >= (*it) * featureMult)
+                                ++correctPlusOnes;
+                            else
+                                ++wrongPlusOnes;
+                        } else {
+                            assert(false);
+                        }
+                    }
+
+                    double pCm1 = static_cast<double>(minusOnesCount) / sampleIndexes.size();  // p(c_-1)
+                    double pCp1 = static_cast<double>(plusOnesCount)  / sampleIndexes.size();  // p(c_+1)
+                    double pTl = static_cast<double>(correctMinusOnes + wrongPlusOnes) / sampleIndexes.size();  // p(t_left)
+                    double pTr = static_cast<double>(correctPlusOnes + wrongMinusOnes) / sampleIndexes.size();  // p(t_right)
+                    double pCm1Tl = (static_cast<double>(correctMinusOnes) / sampleIndexes.size()) / pTl;  // p(c_-1 | t_left)
+                    double pCp1Tl = (static_cast<double>(correctPlusOnes)  / sampleIndexes.size()) / pTl;  // p(c_+1 | t_left)
+                    double pCm1Tr = (static_cast<double>(correctMinusOnes) / sampleIndexes.size()) / pTr;  // p(c_-1 | t_right)
+                    double pCp1Tr = (static_cast<double>(correctPlusOnes)  / sampleIndexes.size()) / pTr;  // p(c_+1 | t_right)
+                    double gini = 1.0 - pCm1 * pCm1 - pCp1 * pCp1 - pTl * (pCm1Tl * (1 - pCm1Tl) + pCp1Tl * (1 - pCp1Tl)) - pTr * (pCm1Tr * (1 - pCm1Tr) + pCp1Tr * (1 - pCp1Tr));
+                    if (gini > bestGini) {
+                        bestGini = gini;
+                        fIndex = featureIndex;
+                        fDivVal = *it;
+                        fMult = featureMult;
+                    }
                 }
             }
         }
 
         assert(fIndex != -1);
 
-        left = new Tree(...);
-        right = new Tree(...);
+        std::vector<int> leftSampleIndexes;
+        std::vector<int> rightSampleIndexes;
+
+        for (size_t i = 0; i < sampleIndexes.size(); ++i) {
+            int sampleIndex = sampleIndexes[i];
+            if (trainData[sampleIndex][fIndex] * fMult < fDivVal * fMult)
+                leftSampleIndexes.push_back(sampleIndex);
+            else
+                rightSampleIndexes.push_back(sampleIndex);
+        }
+
+        left = new Tree(leftSampleIndexes);
+        right = new Tree(rightSampleIndexes);
+    }
+
+    ~Tree() {
+        if (left)
+            delete left;
+        if (right)
+            delete right;
+    }
+
+    int classify(std::vector const& test) {
+        if (leaf)
+            return fMult;
+
+        if (test[fIndex] * fMult < fDivVal * fMult)
+            return left->classify(test);
+        else
+            return right->classify(test);
     }
 
 private:
@@ -88,8 +154,9 @@ private:
     // Если не лист
     Tree* left;
     Tree* right;
-    int fIndex;
-    int fDivVal;
+    int fIndex;   // индекс фичи, по которой происходит разделение
+    int fDivVal;  // значение фичи, по которому происходит разделение
+    int fMult;    // определяет, значение фичи должно быть больше или меньше (а если лист, то хранит результат)
 };
 
 int main() {
@@ -107,6 +174,37 @@ int main() {
     for (int i = 0; i < TREES_COUNT; ++i) {
         trees.push_back(new Tree(sampleIndexes));
     }
+
+    std::vector<int> testData[100];
+    int testLabels[100];
+    readData("arcene_valid.data", testData);
+    readLabels("arcene_valid.labels", testLabels);
+
+    int correct = 0;
+    for (int test = 0; test < 100; ++test) {
+        int treesVotedMinusOne = 0;
+        int treesVotedPlusOne = 0;
+        for (int i = 0; i < TREES_COUNT; ++i) {
+            int res = trees[i]->classify(testData[test]);
+            if (res == -1) {
+                ++treesVotedMinusOne;
+            } else (res == 1) {
+                ++treesVotedPlusOne;
+            } else {
+                assert(false);
+            }
+        }
+
+        finalRes = (treesVotedMinusOne > treesVotedPlusOne) ? -1 : 1;
+        if (testLabels[test] == finalRes) {
+            ++correct;
+        }
+    }
+
+    printf("Correct = %d\nAll = %d\nResult = %lf\n", correct, 100, correct / 100.0);
+
+    for (int i = 0; i < TREES_COUNT; ++i)
+        delete trees[i];
 
     return 0;
 }
