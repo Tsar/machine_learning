4701617d70045a8652860a7d4c4d27b06eed7bf8
Ioann Volkov <volkov.ioann@gmail.com>
2014-11-15 17:20:33 +0300

    PCA almost completed. Do not understand what more I need

--- practice_3/pca.py
+++ practice_3/pca.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
+import sys
 import numpy
 
 if __name__ == "__main__":
     for fileName in ["newBasis" + str(i) for i in range(1, 4)]:
+        print("=== %s ===" % fileName)
         L = []
         with open(fileName, "r") as f:
             for line in f:
                 L.append([float(x) for x in line.split()])
         M = numpy.matrix(L)
+
+        for i in range(200):
+            M[:,i] -= numpy.mean(M[:,i])
+
         MT = M.transpose()
-        print(M)
-        print("------------")
-        print(MT)
-        print("============================")
+        U, s, V = numpy.linalg.svd(MT)
+
+        print(s)
+        print()
