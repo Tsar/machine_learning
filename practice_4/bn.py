47548b31c457fd5685a02e6404fd3fab6920b2f8
Ioann Volkov <volkov.ioann@gmail.com>
2014-12-26 20:39:31 +0300

    Some part of task on bayes networks. Completion is not expected

--- practice_4/bn.py
+++ practice_4/bn.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
 
+g = {}  # graph
+gBack = {}  # graph
+prob = {}
+condP = {}
+
+def calcProb(u):
+    global prob
+
+    if u in gBack:
+        for v in gBack[u]:
+            calcProb(v)
+
+        p_ = 0
+        for l in condP[u]:
+            print("===")
+            print(u)
+            print(l)
+            print(condP[u][l])
+            p = condP[u][l]
+            for x in l:
+                if x.startswith('¬'):
+                    p *= (1 - prob[int(x[-1])])
+                else:
+                    p *= prob[int(x)]
+            p_ += p
+        prob[u] = p_
+
 if __name__ == "__main__":
     with open("bayesian_network", "rb") as f:
         lines = f.read().decode("ISO-8859-15").replace("\r\n", "\n").split("\n")
@@ -8,10 +35,8 @@ if __name__ == "__main__":
     while (i < len(lines)) and (len(lines[i]) > 0):
         vp = lines[i].replace(":", " : ").split()  # [vertex, ':', probability]
         assert((len(vp) == 3) and (vp[1] == ':'))
-        # TODO
+        prob[int(vp[0])] = float(vp[2])
         i += 1
-
-    g = {}  # graph
 
     while i < len(lines):
         i += 1
@@ -23,22 +48,32 @@ if __name__ == "__main__":
                 assert((len(vv) >= 2) and (vv[1] == ':'))
                 u = int(vv[0])
                 g[u] = []
+                condP[u] = {}
                 for j in range(2, len(vv)):
                     v = int(vv[j])
                     print("Edge (%d, %d)" % (u, v))
                     g[u].append(v)
+                    if not v in gBack:
+                        gBack[v] = []
+                    gBack[v].append(u)
             else:
                 cp = lines[i].replace(":", " : ").split()  # [(not?) vertex, (not?) vertex, ..., ':', probability]
                 assert((len(cp) >= 3) and (cp[-2] == ':'))
-                # TODO
+                condP[u][tuple(cp[:-2])] = float(cp[-1])
             i += 1
 
     print(g)
+    print(gBack)
+    print(prob)
+
+    for u in g:
+        calcProb(u)
+        print(prob)
 
     with open("bayesian_network.dot", "w") as f:
         f.write("digraph bn {\n")
         for u in g:
-            f.write("    V%d [label=%s]\n" % (u, str(u)))
+            f.write("    V%d [label=%s]\n" % (u, str(u) + "_" + str(prob[u])))
         for u in g:
             for v in g[u]:
                 f.write("    V%d -> V%d\n" % (u, v))
